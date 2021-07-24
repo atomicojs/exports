46ae841156adc01bbe9b541537da0554a5d6f67a
UpperCod <mtrujillo@outlook.cl>
2021-07-24 01:44:06 -0400

    now publish the *.d.ts files

---
+++ assets.d.ts
@@ -0,0 +1,77 @@
+declare module "*.css" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.png" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.jpg" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.jpeg" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.gif" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.webp" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.svg" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.mp4" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.ogg" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.mp3" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.wav" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.flac" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.aac" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.woff" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.woff2" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.eot" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.ttf" {
+    const Asset: any;
+    export default Asset;
+}
+declare module "*.otf" {
+    const Asset: any;
+    export default Asset;
+}
+
+declare module "*.json" {
+    const Asset: any;
+    export default Asset;
+}
