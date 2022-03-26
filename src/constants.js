e3219a23061bec7b83712c5254c02b9b3a2731b9
UpperCod <mtrujillo@outlook.cl>
2022-03-26 01:05:36 -0300

    fix typesVersions

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,12 @@ export const TS_CONFIG_FIXED = {
     declaration: true,
     emitDeclarationOnly: true,
     outDir: "./types",
+    listEmittedFiles: true,
 };
 
 export const TS_CONFIG = {
     moduleResolution: "Node",
     target: "ESNext",
-    listEmittedFiles: true,
     strict: true,
     jsx: "react-jsx",
     jsxImportSource: "atomico",
