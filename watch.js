5c2c671c11310f43fb4d23c5d81bfdd5cc1b4911
UpperCod <mtrujillo@outlook.cl>
2022-06-01 00:12:38 -0400

    unused

--- watch.js
+++
@@ -1,9 +0,0 @@
-import { prepare } from "./src/module.js";
-
-prepare({
-    src: "./tests/atomico.jsx",
-    dist: "./dist",
-    // analyzer: true,
-    // exports: true,
-    // types: true,
-});
